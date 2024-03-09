28e44906294553ffb5051763d765986785ccbe6a
Lisawakhidah <wakhidahlisa@gmail.com>
2024-03-09 21:54:52 +0700

    Belajar Bahasa Pemrograman Dart Dasar

---
+++ dasarflutter/bin/mtk.dart
@@ -0,0 +1,21 @@
+void main(List<String> arguments){
+  // double number;
+
+  // number = 3 + 2;
+
+  // int number2 = 7 % 3;
+  
+  // print(number2);
+
+  int number = 7;
+  int number2 = 7;
+
+  int result = number ++; //untuk operator assignment dahulu setelah baru di increment 
+  int result2 = ++number2;// diincrement dulu baru assignment contoh 1+7=8
+
+  print(result);
+  print(result2);
+
+  print(number);
+  print(number2);
+}
